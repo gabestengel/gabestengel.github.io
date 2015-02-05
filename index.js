288f92cfb874db4a633e2f960cd01457a523997f
gabe224 <GabrielStengel@Gabriels-MacBook-Pro-28.local>
2015-02-04 21:14:54 -0500

    Revert 2d2fb6c..0f2ad82
    This rolls back to commit 2d2fb6c5cf9d6f531cfa7b3d5450119c433a8d81.

---
+++ index.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    
+    startTests();
+});
+
+function startTests(){
+QUnit.test( "1", function(assert) {
+  assert.deepEqual( triangleExists(1,2,3), true );
+});
+QUnit.test( "2", function(assert) {
+  assert.deepEqual( triangleExists(2,2,2), true );
+});
+QUnit.test( "3", function(assert) {
+  assert.deepEqual( triangleExists(100,50,50), false );
+});
+QUnit.test( "4", function(assert) {
+  assert.deepEqual( triangleExists(0,99,3), true );
+});
+QUnit.test( "5", function(assert) {
+  assert.deepEqual( triangleExists(-1,2,3), true );
+});
+}
+
+function triangleExists(side1,side2,side3){
+    var sides= [side1,side2,side3];
+    sides.sort();
+    if(sides[2]+sides[1]>sides[0]){
+        return true;
+    }
+    return false;
+}
+
+function triangleType(side1,side2,side3){
+    if(side1*side1+side2*side2==side3*side3){
+        return "right";
+    }
+    else if(side1*side1+side2*side2>side3*side3){
+        return "acute";
+    }
+    else if(side1*side1+side2*side2<side3*side3){
+        return "obtuse";
+    }
+}
+
+            
+//-triangleExists(a,b,c), which returns true if a triangle with side lengths a,b,c exists
+//-triangleType(a,b,c), which returns “right”, “obtuse”, or “acute” 

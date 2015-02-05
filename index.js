c7f270dfbff9912817b090a1ff0e5eff9ee9cdee
gabe224 <GabrielStengel@Gabriels-MacBook-Pro-28.local>
2015-02-04 21:08:19 -0500

    k

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
             
-//-triangleExists(a,b,c), which returns true if a triangle with side lengths a,b,c exists
-//-triangleType(a,b,c), which returns “right”, “obtuse”, or “acute” 
